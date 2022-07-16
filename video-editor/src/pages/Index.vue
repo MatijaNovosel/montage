a87191d98965c5181714cffd6c9c22c7931a0cdb
Matija Novosel <mnovosel5@gmail.com>
2022-07-17 00:48:22 +0200

    Main layout

--- video-editor/src/pages/Index.vue
+++ video-editor/src/pages/Index.vue
@@ -1,6 +1,17 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1>Video editor</h1>
+  <q-page class="row">
+    <div class="col-6 bg-green text-center">
+      <h1>1</h1>
+    </div>
+    <div class="col-6 bg-red text-center items-center">
+      <q-video :ratio="16 / 9" src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+    </div>
+    <div class="col-6 bg-yellow text-center">
+      <h1>3</h1>
+    </div>
+    <div class="col-6 bg-blue text-center">
+      <h1>4</h1>
+    </div>
   </q-page>
 </template>
 
