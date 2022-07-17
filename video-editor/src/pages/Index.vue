5c2a2fc0074da007380e930e30682abbf3476458
Matija Novosel <mnovosel5@gmail.com>
2022-07-17 22:42:57 +0200

    Split panes fixes

--- video-editor/src/pages/Index.vue
+++ video-editor/src/pages/Index.vue
@@ -1,15 +1,18 @@
 <template>
   <q-page style="background-color: white" ref="page">
-    <Splitpanes :style="splitPanesStyle">
-      <Pane style="background-color: #7e57c2" min-size="20"> 1 </Pane>
+    <Splitpanes :style="splitPanesStyle" horizontal>
       <Pane>
-        <Splitpanes horizontal>
-          <Pane style="background-color: #e53935">2</Pane>
-          <Pane style="background-color: #64b5f6">3</Pane>
-          <Pane style="background-color: #d4e157">4</Pane>
+        <Splitpanes>
+          <Pane min-size="20" style="background-color: #e53935">1</Pane>
+          <Pane min-size="20" style="background-color: #64b5f6">2</Pane>
         </Splitpanes>
       </Pane>
-      <Pane style="background-color: #ffd54f">5</Pane>
+      <Pane>
+        <Splitpanes>
+          <Pane min-size="20" style="background-color: #e53935">3</Pane>
+          <Pane min-size="20" style="background-color: #64b5f6">4</Pane>
+        </Splitpanes>
+      </Pane>
     </Splitpanes>
   </q-page>
 </template>
@@ -40,3 +43,37 @@ export default defineComponent({
   }
 });
 </script>
+
+<style>
+.splitpanes__splitter {
+  background-color: #ccc;
+  position: relative;
+}
+
+.splitpanes__splitter:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: opacity 0.4s;
+  background-color: rgba(255, 0, 0, 0.3);
+  opacity: 0;
+  z-index: 1;
+}
+
+.splitpanes__splitter:hover:before {
+  opacity: 1;
+}
+
+.splitpanes--vertical > .splitpanes__splitter:before {
+  left: -30px;
+  right: -30px;
+  height: 100%;
+}
+
+.splitpanes--horizontal > .splitpanes__splitter:before {
+  top: -30px;
+  bottom: -30px;
+  width: 100%;
+}
+</style>
