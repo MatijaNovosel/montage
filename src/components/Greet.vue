b6ac0bd798a7486d4393a9040970fb80ca9c7450
Matija Novosel <mnovosel5@gmail.com>
2022-09-15 15:04:50 +0200

    Tweaks

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="card">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
+  <div class="flex items-center justify-center">
+    <input
+      v-model="name"
+      placeholder="Enter a name..."
+      class="border rounded-lg px-3 py-2 text-sm w-full"
+    />
+    <custom-button class="ml-3" text="Greet" @click="greet" />
   </div>
-  <p>{{ greetMsg }}</p>
+  <p class="text-white text-sm mt-5">{{ greetMsg }}</p>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import CustomButton from "./CustomButton.vue";
 
 const greetMsg = ref("");
 const name = ref("");
 
-async function greet() {
+const greet = async () => {
   greetMsg.value = await invoke("greet", { name: name.value });
-}
+};
 </script>
